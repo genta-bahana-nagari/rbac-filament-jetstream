4104b2f25b6bac1b0be31f71ba34dcf9ee6a728f
genta-bahana-nagari <gentapossenti@gmail.com>
2025-03-24 19:11:40 +0700

    revisi post resource

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -45,11 +45,10 @@ class PostResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
