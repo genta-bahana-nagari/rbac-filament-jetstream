6a50f07e8465bca15f4a12824c42124765f324de
genta-bahana-nagari <gentapossenti@gmail.com>
2025-03-24 11:18:33 +0700

    filament user done

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Admin\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\Pages\{CreateRecord, EditRecord, ListRecords};
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Select;
+use Spatie\Permission\Models\Role;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            Forms\Components\TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+            Forms\Components\TextInput::make('email')
+                ->email()
+                ->required()
+                ->maxLength(255),
+
+            Forms\Components\TextInput::make('password')
+                ->password()
+                ->required()
+                ->maxLength(255)
+                ->dehydrateStateUsing(fn ($state) => bcrypt($state))
+                ->hiddenOn('edit'), // Jangan tampilkan password di edit
+
+            Select::make('roles')
+                ->label('Role')
+                ->options(Role::all()->pluck('name', 'name')) // Ambil daftar role dari database
+                ->multiple()
+                ->preload()
+                ->searchable()
+                ->relationship('roles', 'name'), // Hubungkan ke role user                
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            TextColumn::make('name')->sortable()->searchable(),
+            TextColumn::make('email')->sortable()->searchable(),
+            TextColumn::make('roles.name') // Menampilkan role user
+                ->label('Roles')
+                ->badge()
+                ->sortable(),
+        ])
+        ->filters([
+            // Tambahkan filter jika diperlukan
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make(),
+            Tables\Actions\DeleteAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\DeleteBulkAction::make(),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => UserResource\Pages\ListUsers::route('/'),
+            'create' => UserResource\Pages\CreateUser::route('/create'),
+            'edit' => UserResource\Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
